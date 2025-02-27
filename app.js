472546d78dec46c026d42cb88db2fa3fe7fe9ae3
Tolu john <104234518+Tolujoh-n@users.noreply.github.com>
2025-02-27 06:52:26 +0100

    deployment ready v1

--- app.js
+++ app.js
@@ -1,40 +1,43 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const rootRouter = require("./src/router/index");
-const User = require('./src/models/User');
+const User = require("./src/models/User");
 const cookieParser = require("cookie-parser");
-const FullTimeJob = require('./src/models/FullTimeJob');
+const FullTimeJob = require("./src/models/FullTimeJob");
 
 const app = express();
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 app.use(
   cors({
-      origin: "http://localhost:3000", 
-      credentials: true,
+    origin: "https://worklobapp.netlify.app/",
+    credentials: true,
   })
 );
 app.use(cookieParser());
 
-mongoose.connect('mongodb://localhost:27017/authDB')
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.log(err));
+mongoose
+  .connect(
+    "mongodb+srv://tolujohnofficial:sdc3CQEtFuwsYTtE@worklob.p3q3s.mongodb.net/?retryWrites=true&w=majority&appName=worklob"
+  )
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
 
-  app.get('/allUsers', async (req, res) => {
-   try {
-     const users = await User.find({}, '-password'); 
-     res.status(200).json(users); 
-   } catch (e) {
-     console.error("Error fetching users:", e.message); 
-     res.status(500).json({ msg: "Error occurred while fetching users", error: e.message }); 
-   }
- });
+app.get("/allUsers", async (req, res) => {
+  try {
+    const users = await User.find({}, "-password");
+    res.status(200).json(users);
+  } catch (e) {
+    console.error("Error fetching users:", e.message);
+    res
+      .status(500)
+      .json({ msg: "Error occurred while fetching users", error: e.message });
+  }
+});
 
- 
 app.use("/api/v1", rootRouter);
 
-
 app.listen(8080, () => {
-    console.log('Server running on port 8080');
+  console.log("Server running on port 8080");
 });
