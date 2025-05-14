ee54f70759bb17feda437612a21e2d1ce6f288da
Tolu john <104234518+Tolujoh-n@users.noreply.github.com>
2025-05-14 20:44:36 +0100

    on base

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+global.crypto = require("crypto").webcrypto;
+
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
@@ -6,6 +8,17 @@ const rootRouter = require("./src/router/index");
 const User = require("./src/models/User");
 const cookieParser = require("cookie-parser");
 const FullTimeJob = require("./src/models/FullTimeJob");
+const cron = require("node-cron");
+const { updateEthPrice } = require("./src/router/ethPrice");
+
+// Update ETH price every 5 minutes
+cron.schedule("*/5 * * * *", async () => {
+  console.log("Updating ETH price from CoinGecko...");
+  await updateEthPrice();
+});
+
+// Fetch once on startup
+updateEthPrice();
 
 const app = express();
 app.use(bodyParser.json());
@@ -19,10 +32,16 @@ app.use(cookieParser());
 
 mongoose
   .connect(
-    "mongodb+srv://tolujohnofficial:sdc3CQEtFuwsYTtE@worklob.p3q3s.mongodb.net/?retryWrites=true&w=majority&appName=worklob"
+    "mongodb+srv://tolujohnofficial:sdc3CQEtFuwsYTtE@worklob.p3q3s.mongodb.net/?retryWrites=true&w=majority&appName=worklob",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 50000, // Increase to 50 seconds
+      socketTimeoutMS: 45000, // Increase to 45 seconds
+    }
   )
   .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.log(err));
+  .catch((err) => console.error("Mongoose connection error: ", err));
 
 app.get("/allUsers", async (req, res) => {
   try {
@@ -38,6 +57,6 @@ app.get("/allUsers", async (req, res) => {
 
 app.use("/api/v1", rootRouter);
 
-app.listen(8080, () => {
-  console.log("Server running on port 8080");
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
 });
